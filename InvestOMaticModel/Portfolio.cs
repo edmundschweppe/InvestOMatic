8bdb635c585bfbc9a64f38db433dc01056683413
Edmund Schweppe <schweppe@ieee.oreg>
2016-01-21 10:25:48 -0500

    added event handlers to Portfolio to pick up Position.Amount changes

--- InvestOMaticModel/Portfolio.cs
+++ InvestOMaticModel/Portfolio.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -11,6 +13,12 @@ namespace InvestOMaticModel
     public class Portfolio : ObservableObject
     {
         private ObservableCollection<Position> _positions = null;
+
+        public Portfolio()
+        {
+            Positions = new ObservableCollection<Position>();
+        }
+
         public ObservableCollection<Position> Positions
         {
             get
@@ -21,25 +29,99 @@ namespace InvestOMaticModel
             {
                 if (_positions != value)
                 {
+                    if (_positions != null)
+                    {
+                        foreach (Position p in _positions)
+                        {
+                            p.PropertyChanged -= PositionPropertyChangedHandler;
+                        }
+                        _positions.CollectionChanged -= _positions_CollectionChanged;
+                    }
                     _positions = value;
-                    //_positions.CollectionChanged += _positions_CollectionChanged;
-                    RaisePropertyChanged();
+                    if (_positions != null)
+                    {
+                        _positions.CollectionChanged += _positions_CollectionChanged;
+                        foreach (Position p in _positions)
+                        {
+                            p.PropertyChanged += PositionPropertyChangedHandler;
+                        }
+                    }
+                    // We *could* be clever, check to see if TotalValues really changed,
+                    // and only report Positions changing if TotalValues stayed the same,
+                    // but it's simpler to just assume TotalValue will change as well
+                    RaisePropertyChanged(String.Empty);
                 }
             }
         }
 
-        //void _positions_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        //{
-        //    RaisePropertyChanged("TotalValue");
-        //}
+        private void _positions_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    HandlePositionAddEvent(e.NewItems);
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    // don't care
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    HandlePositionRemoveEvent(e.OldItems);
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    HandlePositionAddEvent(e.NewItems);
+                    HandlePositionRemoveEvent(e.OldItems);
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    HandlePositionAddEvent(e.NewItems);
+                    HandlePositionRemoveEvent(e.OldItems);
+                    break;
+                default:
+                    throw new Exception(String.Format("unexpected action type {0}", e.Action));
+            }
+            // We *could* be clever, check to see if TotalValues really changed,
+            // and only report Positions changing if TotalValues stayed the same,
+            // but it's simpler to just assume TotalValue will change as well
+            RaisePropertyChanged(String.Empty);
+        }
+
+        private void HandlePositionAddEvent(IList newItems)
+        {
+            foreach(object newItem in newItems)
+            {
+                Position p = (Position)newItem;
+                p.PropertyChanged += PositionPropertyChangedHandler;
+            }
+        }
+
+        private void HandlePositionRemoveEvent(IList oldItems)
+        {
+            foreach (object oldItem in oldItems)
+            {
+                Position p = (Position)oldItem;
+                p.PropertyChanged -= PositionPropertyChangedHandler;
+            }
+        }
+
+        private void PositionPropertyChangedHandler(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Amount")
+            {
+                RaisePropertyChanged("TotalValue");
+            }
+        }
 
         public double TotalValue
         {
             get
             {
+                if (Positions == null)
+                {
+                    return 0.0;
+                }
                 return Positions.Sum(p => p.Amount);
             }
         }
+
         public void Recalculate(double newValue)
         {
             double amountPerPortfolio = newValue / Positions.Count();
---
+++ UnitTests/PortfolioTests.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InvestOMaticModel;
+using System.ComponentModel;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PortfolioTests
+    {
+
+        #region Tests
+
+        [TestMethod]
+        public void TotalValueZeroForEmptyPortfolio()
+        {
+            double expectedAmount = 0.0;
+            Portfolio target = new Portfolio();
+
+            Assert.AreEqual(expectedAmount, target.TotalValue);
+        }
+
+        [TestMethod]
+        public void OnePosition_TotalValue()
+        {
+            double expectedAmount = 12345.67;
+            Position pos = new Position { SecurityIdentifier = "foo", Amount = expectedAmount };
+            Portfolio target = new Portfolio();
+
+            target.Positions.Add(pos);
+
+            Assert.AreEqual(expectedAmount, target.TotalValue);
+        }
+
+        [TestMethod]
+        public void TwoPositions_TotalValue()
+        {
+            Position posOne = new Position { SecurityIdentifier = "foo", Amount = 12345.67 };
+            Position posTwo = new Position { SecurityIdentifier = "bar", Amount = 23456.78 };
+            double expectedAmount = posOne.Amount + posTwo.Amount;
+            Portfolio target = new Portfolio();
+
+            target.Positions.Add(posOne);
+            target.Positions.Add(posTwo);
+
+            Assert.AreEqual(expectedAmount, target.TotalValue);
+        }
+
+        [TestMethod]
+        public void ChangingPositionAmountChangesTotalValue()
+        {
+            Position posOne = new Position { SecurityIdentifier = "foo", Amount = 12345.67 };
+            Position posTwo = new Position { SecurityIdentifier = "bar", Amount = 23456.78 };
+            Portfolio target = new Portfolio();
+            target.Positions.Add(posOne);
+            target.Positions.Add(posTwo);
+
+            posTwo.Amount = 34567.89;
+
+            double expectedAmount = posOne.Amount + posTwo.Amount;
+            Assert.AreEqual(expectedAmount, target.TotalValue);
+        }
+
+        [TestMethod]
+        public void AddingPositionFiresPropertyChangedEvent()
+        {
+            Position posOne = new Position { SecurityIdentifier = "foo", Amount = 12345.67 };
+            Position posTwo = new Position { SecurityIdentifier = "bar", Amount = 23456.78 };
+            Portfolio target = new Portfolio();
+            target.Positions.Add(posOne);
+
+            List<string> propertyNames = new List<string>();
+            target.PropertyChanged += (s, e) =>
+            {
+                propertyNames.Add(e.PropertyName);
+            };
+
+            target.Positions.Add(posTwo);
+
+            Assert.AreEqual(1, propertyNames.Count(), "checking propertyNames Count");
+            Assert.IsTrue(propertyNames.Contains(String.Empty), "checking that propertyNames contains empty string");
+        }
+
+        [TestMethod]
+        public void RemovingPositionFiresPropertyChangedEvent()
+        {
+            Position posOne = new Position { SecurityIdentifier = "foo", Amount = 12345.67 };
+            Position posTwo = new Position { SecurityIdentifier = "bar", Amount = 23456.78 };
+            Portfolio target = new Portfolio();
+            target.Positions.Add(posOne);
+            target.Positions.Add(posTwo);
+
+            List<string> propertyNames = new List<string>();
+            target.PropertyChanged += (s, e) =>
+            {
+                propertyNames.Add(e.PropertyName);
+            };
+
+            target.Positions.Remove(posTwo);
+
+            Assert.AreEqual(1, propertyNames.Count(), "checking propertyNames Count");
+            Assert.IsTrue(propertyNames.Contains(String.Empty), "checking that propertyNames contains empty string");
+        }
+
+        [TestMethod]
+        public void ReplacingPositionsCollectionFiresPropertyChangedEvent()
+        {
+            Position posOne = new Position { SecurityIdentifier = "foo", Amount = 12345.67 };
+            Position posTwo = new Position { SecurityIdentifier = "bar", Amount = 23456.78 };
+            Position posThree = new Position { SecurityIdentifier = "bletch", Amount = 34567.89 };
+            ObservableCollection<Position> origPositions = new ObservableCollection<Position>{
+                posOne,
+                posTwo
+            };
+            ObservableCollection<Position> newPositions = new ObservableCollection<Position> { posThree };
+            Portfolio target = new Portfolio { Positions = origPositions };
+
+            List<string> propertyNames = new List<string>();
+            target.PropertyChanged += (s, e) =>
+            {
+                propertyNames.Add(e.PropertyName);
+            };
+
+            target.Positions = newPositions;
+
+            Assert.AreEqual(1, propertyNames.Count(), "checking propertyNames Count");
+            Assert.IsTrue(propertyNames.Contains(String.Empty), "checking that propertyNames contains String.Empty");
+        }
+
+        [TestMethod]
+        public void ChangingPositionAmountFiresTotalValuePropertyChangedEvent()
+        {
+            Position posOne = new Position { SecurityIdentifier = "foo", Amount = 12345.67 };
+            Position posTwo = new Position { SecurityIdentifier = "bar", Amount = 23456.78 };
+            Portfolio target = new Portfolio();
+            target.Positions.Add(posOne);
+            target.Positions.Add(posTwo);
+
+            List<string> propertyNames = new List<string>();
+            target.PropertyChanged += (s, e) =>
+            {
+                propertyNames.Add(e.PropertyName);
+            };
+
+            posTwo.Amount = 34567.89;
+
+            Assert.AreEqual(1, propertyNames.Count(), "checking propertyNames Count");
+            Assert.AreEqual("TotalValue", propertyNames[0], "checking propertyNames[0]");
+        }
+
+        [TestMethod]
+        public void ChangingRemovedPositionDoesNotFirePropertyChangedEvent()
+        {
+            Position posOne = new Position { SecurityIdentifier = "foo", Amount = 12345.67 };
+            Position posTwo = new Position { SecurityIdentifier = "bar", Amount = 23456.78 };
+            Portfolio target = new Portfolio();
+            target.Positions.Add(posOne);
+            target.Positions.Add(posTwo);
+
+            target.Positions.Remove(posTwo);
+
+            List<string> propertyNames = new List<string>();
+            target.PropertyChanged += (s, e) =>
+            {
+                propertyNames.Add(e.PropertyName);
+            };
+
+            posTwo.Amount = 34567.89;
+
+            Assert.AreEqual(0, propertyNames.Count(), "checking propertyNames Count");
+        }
+
+        [TestMethod]
+        public void ChangingPositionAmountInNewCollectionFiresPropertyChangedEvent()
+        {
+            Position posOne = new Position { SecurityIdentifier = "foo", Amount = 12345.67 };
+            Position posTwo = new Position { SecurityIdentifier = "bar", Amount = 23456.78 };
+            Position posThree = new Position { SecurityIdentifier = "bletch", Amount = 34567.89 };
+            ObservableCollection<Position> origPositions = new ObservableCollection<Position>{
+                posOne,
+                posTwo
+            };
+            ObservableCollection<Position> newPositions = new ObservableCollection<Position> { posThree };
+            Portfolio target = new Portfolio { Positions = origPositions };
+            target.Positions = newPositions;
+
+            List<string> propertyNames = new List<string>();
+            target.PropertyChanged += (s, e) =>
+            {
+                propertyNames.Add(e.PropertyName);
+            };
+
+            posThree.Amount = 45678.90;
+
+            Assert.AreEqual(1, propertyNames.Count(), "checking propertyNames Count");
+            Assert.IsTrue(propertyNames.Contains("TotalValue"), "checking that propertyNames contains TotalValue");
+        }
+
+        [TestMethod]
+        public void ChangingPositionAmountInOldCollectionDoesNotFirePropertyChangedEvent()
+        {
+            Position posOne = new Position { SecurityIdentifier = "foo", Amount = 12345.67 };
+            Position posTwo = new Position { SecurityIdentifier = "bar", Amount = 23456.78 };
+            Position posThree = new Position { SecurityIdentifier = "bletch", Amount = 34567.89 };
+            ObservableCollection<Position> origPositions = new ObservableCollection<Position>{
+                posOne,
+                posTwo
+            };
+            ObservableCollection<Position> newPositions = new ObservableCollection<Position> { posThree };
+            Portfolio target = new Portfolio { Positions = origPositions };
+            target.Positions = newPositions;
+
+            List<string> propertyNames = new List<string>();
+            target.PropertyChanged += (s, e) =>
+            {
+                propertyNames.Add(e.PropertyName);
+            };
+
+            posOne.Amount = 45678.90;
+
+            Assert.AreEqual(0, propertyNames.Count(), "checking propertyNames Count");
+        }
+        #endregion Tests
+    }
+}
